a8897cbbde8924eff757f01ca495b111f08a60ad
ivan <ivieee@yandex.ru>
2020-09-30 21:35:27 +0400

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let num = String(n)
+    if (
+        String(n).slice(0, 1) === '-'
+        || String(n).slice(0, 1) === '0'
+    ) {
+        num = String(n).slice(1)
+    }
+    return num.split('').reverse().join('')
 }
